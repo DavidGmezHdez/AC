46371a060e8c7edb71c609d6d55466117beb2c8f
DavidGmezHdez <davidgmezhdez@gmail.com>
2020-02-25 13:23:25 +0100

    Añadiendo archivos para la ejeución ejercicio 7 | Modificando script | Actualizando tablas

--- Pŕactica 0/ejer7/SumaVectoresTest.sh
+++ Pŕactica 0/ejer7/SumaVectoresTest.sh
@@ -2,7 +2,23 @@
 
 #Testearemos para cada longitud los tres programas
 
+echo "\n\nCALCULO LOCAL\n\n"
 for ((i = 65536 ; i <= 67108864 ; i=i*2)); do
-   echo "Tamaño: $i " ;
-   ./SumaVectoresC $i ;
+   echo "Tamaño Local: $i " ;
+   ./SumaVectoresLocal $i ;
 done
+
+echo "\n\nCALCULO GLOBAL\n\n"
+for ((i = 65536 ; i <= 67108864 ; i=i*2)); do
+   echo "Tamaño Global: $i " ;
+   ./SumaVectoresGlobal $i ;
+done
+
+echo "\n\nCALCULO DINAMICO \n\n"
+for ((i = 65536 ; i <= 67108864 ; i=i*2)); do
+   echo "Tamaño Dinamico: $i " ;
+   ./SumaVectoresDinamico $i ;
+done
+
+
+
