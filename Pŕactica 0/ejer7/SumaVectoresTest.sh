345c8dce62a37e2435b207bb3c2f04ef015fd02f
DavidGmezHdez <davidgmezhdez@gmail.com>
2020-02-26 13:19:14 +0100

    Documentos semi-acabados | Añadir gráficas y gestionar carpetas

--- Pŕactica 0/ejer7/SumaVectoresTest.sh
+++ Pŕactica 0/ejer7/SumaVectoresTest.sh
@@ -2,19 +2,19 @@
 
 #Testearemos para cada longitud los tres programas
 
-echo "\n\nCALCULO LOCAL\n\n"
+printf "\n\nCALCULO LOCAL\n\n"
 for ((i = 65536 ; i <= 67108864 ; i=i*2)); do
    echo "Tamaño Local: $i " ;
    ./SumaVectoresLocal $i ;
 done
 
-echo "\n\nCALCULO GLOBAL\n\n"
+printf "\n\nCALCULO GLOBAL\n\n"
 for ((i = 65536 ; i <= 67108864 ; i=i*2)); do
    echo "Tamaño Global: $i " ;
    ./SumaVectoresGlobal $i ;
 done
 
-echo "\n\nCALCULO DINAMICO \n\n"
+printf "\n\nCALCULO DINAMICO \n\n"
 for ((i = 65536 ; i <= 67108864 ; i=i*2)); do
    echo "Tamaño Dinamico: $i " ;
    ./SumaVectoresDinamico $i ;
